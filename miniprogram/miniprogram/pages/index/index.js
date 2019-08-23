34ee4f77de6bb5f38f28c55e3ae6456d9ef89477
weiwensangsang <weiwensangsang@users.noreply.github.com>
2019-08-23 13:48:27 +0800

    add 3 buttom

--- miniprogram/miniprogram/pages/index/index.js
+++ miniprogram/miniprogram/pages/index/index.js
@@ -3,6 +3,13 @@ const app = getApp()
 
 Page({
   data: {
+    loading: false,
+    color: '#000',
+    background: '#fff',
+    show: true,
+    animated: false,
+    toView: 'red',
+    scrollTop: 100,
     avatarUrl: './user-unlogin.png',
     userInfo: {},
     logged: false,
@@ -49,6 +56,30 @@ Page({
           })
         }
       }
+    })
+  },
+  upper: function (e) {
+    console.log(e)
+  },
+  lower: function (e) {
+    console.log(e)
+  },
+  scroll: function (e) {
+    console.log(e)
+  },
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
     })
   },
 
