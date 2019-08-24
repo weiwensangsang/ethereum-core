0b3469566326f22bf5e4bd1c19a973cca1ada77e
weiwensangsang <weiwensangsang@users.noreply.github.com>
2019-08-24 16:25:27 +0800

    add 云开发

--- miniprogram/miniprogram/pages/develop/develop.js
+++ miniprogram/miniprogram/pages/develop/develop.js
@@ -2,37 +2,55 @@
 Page({
   data: {
     focus: false,
-    inputValue: ''
+    inputValue: '',
+    name: '',
+    vote_A: "23",
+    vote_B: "23",
+    vote_C: "23",
   },
-  bindKeyInput: function (e) {
-    this.setData({
-      inputValue: e.detail.value
+  inputData: function(e) {
+    var value = e.detail.value;
+    switch (e.currentTarget.id) {
+      case "name":
+        this.setData({
+          name: value
+        });
+        break;
+      case "vote_A":
+        this.setData({
+          vote_A: value
+        });
+        break;
+      case "vote_B":
+        this.setData({
+          vote_B: value
+        });
+        break;
+      case "vote_C":
+        this.setData({
+          vote_C: value
+        });
+        break;
+    }
+  },
+  createQusetion: function(e) {
+   
+    let that = this
+    console.log(that.data)
+    wx.cloud.callFunction({
+      // 要调用的云函数名称
+      name: 'createQuestion',
+      // 传递给云函数的event参数
+      data: {
+        name: that.data.name,
+        vote_A: that.data.vote_A,
+        vote_B: that.data.vote_B,
+        vote_C: that.data.vote_C
+      }
+    }).then(res => {
+      console.log(res.result)
+    }).catch(err => {
+      console.log(err)
     })
-  },
-  bindReplaceInput: function (e) {
-    var value = e.detail.value
-    var pos = e.detail.cursor
-    var left
-    if (pos !== -1) {
-      // 光标在中间
-      left = e.detail.value.slice(0, pos)
-      // 计算光标的位置
-      pos = left.replace(/11/g, '2').length
-    }
-
-    // 直接返回对象，可以对输入进行过滤处理，同时可以控制光标的位置
-    return {
-      value: value.replace(/11/g, '2'),
-      cursor: pos
-    }
-
-    // 或者直接返回字符串,光标在最后边
-    // return value.replace(/11/g,'2'),
-  },
-  bindHideKeyboard: function (e) {
-    if (e.detail.value === '123') {
-      // 收起键盘
-      wx.hideKeyboard()
-    }
   }
 })
