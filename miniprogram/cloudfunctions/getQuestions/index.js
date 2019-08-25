f86545d8c7907d641d53ab44d1582c17bb21e47c
weiwensangsang <weiwensangsang@users.noreply.github.com>
2019-08-25 17:40:14 +0800

    Revert "fix page"
    This reverts commit 6b65b0a4d0d01c41fb1568bcfcc636ef6407c875.

--- miniprogram/cloudfunctions/getQuestions/index.js
+++ miniprogram/cloudfunctions/getQuestions/index.js
@@ -5,29 +5,14 @@ cloud.init()
 const db = cloud.database()
 // 云函数入口函数
 exports.main = async(event, context) => {
-  var pageIndex = event.pageIndex;
-  var pageSize = event.pageSize;
-
-  var amount = await db.collection('question')
-    .count()
-    .then(res => {
-      return res.total
-    })
-
-  var pageCount = Math.ceil(amount / pageSize);
-
-  var list = await db.collection('question')
+  return await db.collection('question')
     .orderBy('createTime', 'desc')
-    .skip((pageIndex - 1) * pageSize)
-    .limit(pageSize) // 限制返回数量为 5 条
-    .get().then(res => {
-      return res.data
+    .limit(5) // 限制返回数量为 5 条
+    .get()
+    .then(res => {
+      console.log(res.data)
     })
-
-  return {
-    amount: amount,
-    pageCount: pageCount,
-    pageSize: pageSize,
-    list: list
-  }
+    .catch(err => {
+      console.error(err)
+    })
 }
