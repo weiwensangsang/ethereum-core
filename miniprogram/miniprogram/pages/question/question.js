a8ee0b0a8dbc7ed0caa8b3a7cdb925c89588cbbd
weiwensangsang <weiwensangsang@users.noreply.github.com>
2019-09-05 17:02:49 +0800

    fix

--- miniprogram/miniprogram/pages/question/question.js
+++ miniprogram/miniprogram/pages/question/question.js
@@ -1,7 +1,14 @@
 // miniprogram/pages/develop.js
 Page({
   data: {
-   
+    _id: "暂无数据"
+  },
+
+  onLoad: function(options) {
+    var that = this;
+    that.setData({
+      _id: options._id
+    })
   }
-  
+
 })
