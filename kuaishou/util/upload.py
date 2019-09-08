e4f6080a113c89fddc7411cc2f2312954626e5ef
weiwensangsang <weiwensangsang@users.noreply.github.com>
2019-09-08 23:21:43 +0800

    fix

--- kuaishou/util/upload.py
+++ kuaishou/util/upload.py
@@ -1,6 +1,7 @@
 import os
 
 import googleapiclient.discovery
+import requests
 
 from googleapiclient.http import MediaFileUpload
 
@@ -8,7 +9,7 @@ from googleapiclient.http import MediaFileUpload
 def main():
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "0"
 
     api_service_name = "youtube"
     api_version = "v3"
@@ -16,7 +17,7 @@ def main():
 
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey=DEVELOPER_KEY)
-
+    print()
     request = youtube.channels().list(
         part="snippet,contentDetails,statistics",
         id="UC_x5XG1OV2P6uZZ5FSM9Ttw"
@@ -27,4 +28,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    r = requests.get("https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=UCRr2dbgMHtMq9Z9lbG6GqWA&key=AIzaSyAEfdBhr2EY2z1I7newyVwqFksWjMDjdTY")
+    print(r.content)
