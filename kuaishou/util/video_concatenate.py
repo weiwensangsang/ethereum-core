575262e322743c286cc301a4f531a26a5d115469
weiwensangsang <weiwensangsang@users.noreply.github.com>
2019-09-08 13:44:24 +0800

    fix

--- kuaishou/util/video_concatenate.py
+++ kuaishou/util/video_concatenate.py
@@ -6,9 +6,10 @@ from util.util import time
 class Video():
 
     def __init__(self, id, caption, url, createTime, keyword, page):
+        trans = str.maketrans("<>/\\|:\"*?", "         ")
         self.path_name = ""
         self.id = id
-        self.caption = caption.replace('\n', ' ').replace(' ', '_')
+        self.caption = caption.replace('\n', ' ').translate(trans).replace(' ', '_')
         self.url = url
         self.page = page
         self.keyword = keyword
