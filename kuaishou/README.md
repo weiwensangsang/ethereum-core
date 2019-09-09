d2cc26d2bbc563e756e37921b4d556a9d637156e
weiwensangsang <weiwensangsang@users.noreply.github.com>
2019-09-09 18:34:21 +0800

    fuXX

--- kuaishou/README.md
+++ kuaishou/README.md
@@ -1,5 +1,16 @@
-> ## 向youtube上传视频时要开启翻墙，设置链接转发
->
-> set http_proxy=http://127.0.0.1:1080
+cmd 执行python 会找不到包 python cmd modulenotfounderror:
 
-set https_proxy=http://127.0.0.1:1080
+python可能要32位版本
+
+627035101394-ggt7q1q7pm2pv47nit6hv30udhicup8k.apps.googleusercontent.com
+
+
+https://developers.google.com/apis-explorer/#p/youtube/v3/
+
+https://guozh.net/318/
+
+https://developers.google.cn/youtube/v3/getting-started?hl=zh-tw
+
+apikey 只能用作读
+一定要用oauth2
+如果用官方代码，就必要找到python全局代理的写法，else用request会好很多。
--- kuaishou/util/upload.py
+++ kuaishou/util/upload.py
@@ -3,6 +3,21 @@ import os
 import requests
 
 
-if __name__ == "__main__":
-    r = requests.get("https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=UCRr2dbgMHtMq9Z9lbG6GqWA&key=AIzaSyAEfdBhr2EY2z1I7newyVwqFksWjMDjdTY")
+def test_connection():
+    proxies = {"http": "http://127.0.0.1:1080", "https": "http://127.0.0.1:1080", }
+    r = requests.get(
+        "https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=UCRr2dbgMHtMq9Z9lbG6GqWA&key=AIzaSyAEfdBhr2EY2z1I7newyVwqFksWjMDjdTY",
+        proxies=proxies)
     print(r.content)
+
+
+def upload_video_to_youtube():
+    proxies = {"http": "http://127.0.0.1:1080", "https": "http://127.0.0.1:1080", }
+    r = requests.post(
+        "https://www.googleapis.com/upload/youtube/v3/videos?part=snippet&id=UCRr2dbgMHtMq9Z9lbG6GqWA&key=AIzaSyAEfdBhr2EY2z1I7newyVwqFksWjMDjdTY",
+        proxies=proxies)
+    print(r.content)
+
+
+if __name__ == "__main__":
+    test_connection()
